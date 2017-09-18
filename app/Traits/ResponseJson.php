9b18013fbb20491f9c42d83a060e2440c5860f30
Jerry Zhang <zjk6700@gmail.com>
2017-09-18 17:33:29 +0800

    update

--- app/Traits/ResponseJson.php
+++ app/Traits/ResponseJson.php
@@ -15,13 +15,18 @@ trait ResponseJson{
                 $data['info'] 	= $args[1];
             }
             if(is_array($args[1])){
-                $data = array_merge($data, $args[1]);
+//                $data = array_merge($data, $args[1]);
+                $data['data'] = $args[1];
             }
         }elseif ($argsNum == 3){
             if(is_string($args[0]) && is_string($args[1]) && is_string($args[2])){
                 $data['status'] = $args[0];
                 $data['info'] = $args[1];
                 $data['field'] = $args[2];
+            }elseif (is_string($args[0]) && is_array($args[1]) && is_string($args[2])){
+                $data['status'] = $args[0];
+                $data['data'] = $args[1];
+                $data['info'] = $args[2];
             }
         }
         return response()->json($data);
