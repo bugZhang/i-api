3fce7b610f6a3fa280b477b677591dd2bb5028ac
Jerry Zhang <zjk6700@gmail.com>
2017-09-28 14:24:52 +0800

    添加日志

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use App\Mail\BackupData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
     //
     public function test(){
+//        Redis::set('asdf', 'asd11111f');
+       echo  Redis::get('asdf');
     }
 }
