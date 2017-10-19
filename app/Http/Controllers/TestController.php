fd2e4024a7c93988731a20fec59cd01a93e0e136
Jerry Zhang <zjk6700@gmail.com>
2017-10-19 21:37:16 +0800

    update select

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Mail\BackupData;
+use App\Model\BankModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redis;
 
@@ -13,5 +15,11 @@ class TestController extends Controller
     public function test(){
 //        Redis::set('asdf', 'asd11111f');
 //       echo  Redis::get('asdf');
+        DB::connection()->enableQueryLog();
+
+        $bankModel  = new BankModel();
+        $res = $bankModel->selectBanksByNameAndArea(102, '香港', '山东省', '青岛市');
+        $log = DB::getQueryLog();
+        var_dump($log);
     }
 }
