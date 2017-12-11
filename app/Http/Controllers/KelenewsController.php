d6e289f9bf606b53d3f5e42bb6b235d9f6ef6cdd
Jerry Zhang <zjk6700@gmail.com>
2017-12-11 17:16:33 +0800

    添加缓存

--- app/Http/Controllers/KelenewsController.php
+++ app/Http/Controllers/KelenewsController.php
@@ -4,41 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Model\KelenewsModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\Console\Helper\Helper;
 
 class KelenewsController extends Controller
 {
-    public function getPosts($page){
-        $kelenewsModel  = new KelenewsModel();
-        $posts = $kelenewsModel->selectAllPostsByPage($page ? $page : 1);
 
-        if(count($posts)){
+    private $cache_kelenews_list = 'kelenews_page_';
+    private $cache_kelenews_post = 'kelenews_post_';
 
-            $allTags = $kelenewsModel->selectAllTags()->toArray();
-            foreach ($posts as $post){
-                $post->post_excerpt = $this->getPostExcerpt($post);
-                $post->post_date = date('Y-m-d', strtotime($post->post_date));
+    /**
+     * @param $page 页数
+     */
+    public function getPostsFromCache($page){
+        $page = ($page && $page > 0) ? $page : 1;
+        $post_list_cache_key = $this->cache_kelenews_list . $page;
 
-                preg_match('/<img.*?src="(.*?)"/i', $post->post_content, $matchs);
-                if($matchs){
-                    $post->thumb_pic = $matchs[1];
-                }
+        if(Cache::has($post_list_cache_key)){
 
-                $postTags = $kelenewsModel->selectPostTags($post->ID);
-                if($postTags){
-                    $post_tags = $this->getPostTagsName($postTags->toArray(), $allTags);
-                    $post->post_tags = implode(',', $post_tags);
-                }
-
-                if($viewCount = $kelenewsModel->getViewCount($post->ID)){
-                    $post->view_count = $viewCount;
-                }
-
+            $arrPosts = Cache::get($post_list_cache_key);
+            if($arrPosts){
+                return $this->return_json('success', $arrPosts);
+            }else{
+                return $this->return_json('error', '未查询到数据');
             }
-
-            return $this->return_json('success', $posts->toArray());
         }else{
-            return $this->return_json('error', '未查询到数据');
+            $kelenewsModel  = new KelenewsModel();
+            $posts = $kelenewsModel->selectAllPostsByPage($page);
+
+            if(count($posts)){
+
+                $allTags = $kelenewsModel->selectAllTags()->toArray();
+                foreach ($posts as $post){
+                    $post->post_excerpt = $this->getPostExcerpt($post);
+                    $post->post_date = date('Y-m-d', strtotime($post->post_date));
+
+                    preg_match('/<img.*?src="(.*?)"/i', $post->post_content, $matchs);
+                    if($matchs){
+                        $post->thumb_pic = $matchs[1];
+                    }
+
+                    $postTags = $kelenewsModel->selectPostTags($post->ID);
+                    if($postTags){
+                        $post_tags = $this->getPostTagsName($postTags->toArray(), $allTags);
+                        $post->post_tags = implode(',', $post_tags);
+                    }
+
+                    if($viewCount = $kelenewsModel->getViewCount($post->ID)){
+                        $post->view_count = $viewCount;
+                    }
+                }
+
+                $arrPosts = $posts->toArray();
+                Cache::put($post_list_cache_key, $arrPosts, 30);   //minutes
+                return $this->return_json('success', $arrPosts);
+            }else{
+                return $this->return_json('error', '未查询到数据');
+            }
         }
     }
 
@@ -47,41 +69,55 @@ class KelenewsController extends Controller
         if(!$postId){
             return $this->return_json('error', 'id不能为空');
         }
-        $kelenewsModel  = new KelenewsModel();
+        $cache_key = $this->cache_kelenews_post . $postId;
 
-        $post = $kelenewsModel->selectPostById($postId);
-
-        if(count($post)){
-            $post->post_content = preg_replace('/(<img.*?)width=".*?"/i', '$1', $post->post_content);
-            $post->post_content = preg_replace('/(<img.*?)style=".*?"/i', '$1', $post->post_content);
-            $post->post_content = preg_replace('/<img([^>]+>)/i', '<img width="100%" $1', $post->post_content);
-        }
-
-        $post->post_date = date('Y-m-d', strtotime($post->post_date));
-
-        if($post->is_video = $kelenewsModel->isVideoFormat($postId)){
-            preg_match('/<video.*?src="(.*?)"/i', $post->post_content, $matchs);
-            if($matchs){
-                $post->video_src = $matchs[1];
+        if(Cache::has($cache_key)){
+            $arrPost = Cache::get($cache_key);
+            if($arrPost){
+                return $this->return_json('success', $arrPost);
+            }else{
+                return $this->return_json('error', '未查询到数据');
             }
-            $post->post_content = preg_replace('/<video.*?video>/i', '', $post->post_content);
-        }
-        $postTags = $kelenewsModel->selectPostTags($postId);
-        if(count($postTags)){
-            $allTags = $kelenewsModel->selectAllTags()->toArray();
-            $post_tags = $this->getPostTagsName($postTags->toArray(), $allTags);
-            if($postTags){
-                $post->post_tags = implode(',', $post_tags);
+        }else{
+            $kelenewsModel  = new KelenewsModel();
+            $post = $kelenewsModel->selectPostById($postId);
+
+            if(count($post)){
+                $post->post_content = preg_replace('/(<img.*?)width=".*?"/i', '$1', $post->post_content);
+                $post->post_content = preg_replace('/(<img.*?)style=".*?"/i', '$1', $post->post_content);
+                $post->post_content = preg_replace('/<img([^>]+>)/i', '<img width="100%" $1', $post->post_content);
+            }else{
+                return $this->return_json('error', '未查找到数据');
             }
+
+            $post->post_date = date('Y-m-d', strtotime($post->post_date));
+
+            if($post->is_video = $kelenewsModel->isVideoFormat($postId)){
+                preg_match('/<video.*?src="(.*?)"/i', $post->post_content, $matchs);
+                if($matchs){
+                    $post->video_src = $matchs[1];
+                }
+                $post->post_content = preg_replace('/<video.*?video>/i', '', $post->post_content);
+            }
+            $postTags = $kelenewsModel->selectPostTags($postId);
+            if(count($postTags)){
+                $allTags = $kelenewsModel->selectAllTags()->toArray();
+                $post_tags = $this->getPostTagsName($postTags->toArray(), $allTags);
+                if($postTags){
+                    $post->post_tags = implode(',', $post_tags);
+                }
+            }
+
+            $kelenewsModel->increatViewCount($postId);
+
+            if($viewCount = $kelenewsModel->getViewCount($post->ID)){
+                $post->view_count = $viewCount;
+            }
+
+            $arrPost = $post->toArray();
+            Cache::put($cache_key, $arrPost, 60 * 24);   //minutes
+            return $this->return_json('success', $arrPost);
         }
-
-        $kelenewsModel->increatViewCount($postId);
-
-        if($viewCount = $kelenewsModel->getViewCount($post->ID)){
-            $post->view_count = $viewCount;
-        }
-
-        return $this->return_json('success', $post->toArray());
 
     }
 
