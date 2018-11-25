1a0ed6d964cbdf9cfaffe84a5ec93d607ca148cb
Jerry Zhang <zjk6700@gmail.com>
2018-11-25 16:55:58 +0800

    公众号消息测试

--- app/Http/Controllers/WxTudouController.php
+++ app/Http/Controllers/WxTudouController.php
@@ -9,33 +9,34 @@ use Illuminate\Support\Facades\Log;
 class WxTudouController extends Controller
 {
 
-    public function test1(Request $request) {
-
-        $signature = $request->input('signature');
-        $timestamp = $request->input('timestamp');
-        $nonce     = $request->input('nonce');
-        $echostr   = $request->input('echostr');
-
-        $token = config('app.WX_GZH_TOKEN');
-
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( '', $tmpArr );
-        $tmpStr  = sha1($tmpStr);
-
-        if( $tmpStr == $signature ){
-            return $echostr;
-        }else{
-            return false;
-        }
-
-    }
+//    public function test1(Request $request) {
+//
+//        $signature = $request->input('signature');
+//        $timestamp = $request->input('timestamp');
+//        $nonce     = $request->input('nonce');
+//        $echostr   = $request->input('echostr');
+//
+//        $token = config('app.WX_GZH_TOKEN');
+//
+//        $tmpArr = array($token, $timestamp, $nonce);
+//        sort($tmpArr, SORT_STRING);
+//        $tmpStr = implode( '', $tmpArr );
+//        $tmpStr  = sha1($tmpStr);
+//
+//        if( $tmpStr == $signature ){
+//            return $echostr;
+//        }else{
+//            return false;
+//        }
+//
+//    }
 
 
     public function test(Request $request){
         $msg = $request->getContent();
         Log::info($msg);
         if($msg){
+            Log::info('-=-=-=-=-=-=-=-=');
             $xmlObj = simplexml_load_string($msg, 'SimpleXMLElement', LIBXML_NOCDATA);
             $msgType = $xmlObj->MsgType;
 
@@ -43,29 +44,11 @@ class WxTudouController extends Controller
 
             Log::info($msgType);
             Log::info($content);
+        }else{
+            Log::info('+++++++++++++++');
         }
         return 'success';
 
     }
 
-
 }
-
-
-//private function checkSignature()
-//{
-//    _GET["signature"];
-//    _GET["timestamp"];
-//    _GET["nonce"];
-//
-//    tmpArr = array(timestamp, $nonce);
-//    sort($tmpArr, SORT_STRING);
-//    $tmpStr = implode( $tmpArr );
-//    $tmpStr = sha1( $tmpStr );
-//
-//    if( signature ){
-//        return true;
-//    }else{
-//        return false;
-//    }
-//}
